1ae8df46d6dcad754f4b2df92b096e69f2629675
MelodyJoke <melochanladyjj@gmail.com>
2016-11-23 15:22:44 +0800

    algorithms

--- algorithms.h
+++ algorithms.h
@@ -12,7 +12,7 @@
  * 2.较大数除以较小数的余数， 与较小数的最大公约数为问题的解
  */
 int archimedesAlg(int left, int right) {
-    if (left > right) {
+    if (left >= right) {
         int temp = left;
         left = right;
         right = temp;
